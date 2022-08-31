04be63e0a781f46d38c8f43198dcd61d5cf9cec2
Mikkel Wienberg Madsen <mikkel@princh.com>
2022-08-31 10:05:11 +0200

    disable neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1066,26 +1066,26 @@ return require("packer").startup {
                 vim.g.asciidoctor_folding = 1
             end,
         }
-        use {
-            "nvim-neorg/neorg",
-            config = function()
-                require("neorg").setup {
-                    -- Tell Neorg what modules to load
-                    load = {
-                        ["core.defaults"] = {}, -- Load all the default modules
-                        ["core.norg.concealer"] = {}, -- Allows for use of icons
-                        ["core.norg.dirman"] = { -- Manage your directories with Neorg
-                            config = {
-                                workspaces = {
-                                    my_workspace = "~/neorg",
-                                },
-                            },
-                        },
-                    },
-                }
-            end,
-            requires = "nvim-lua/plenary.nvim",
-        }
+        -- use {
+        --     "nvim-neorg/neorg",
+        --     config = function()
+        --         require("neorg").setup {
+        --             -- Tell Neorg what modules to load
+        --             load = {
+        --                 ["core.defaults"] = {}, -- Load all the default modules
+        --                 ["core.norg.concealer"] = {}, -- Allows for use of icons
+        --                 ["core.norg.dirman"] = { -- Manage your directories with Neorg
+        --                     config = {
+        --                         workspaces = {
+        --                             my_workspace = "~/neorg",
+        --                         },
+        --                     },
+        --                 },
+        --             },
+        --         }
+        --     end,
+        --     requires = "nvim-lua/plenary.nvim",
+        -- }
 
         -- ==========  fish  ==========
         use { "mtoohey31/cmp-fish", ft = "fish" }
