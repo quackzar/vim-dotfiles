fa9bdd04983b259a7a76a5be7393818002b784fd
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2024-09-24 09:20:52 +0200

    fix sessions?

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,6 +10,7 @@ return {
     {
         "folke/persistence.nvim",
         event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        module = "persistence",
         config = true,
     },
 
